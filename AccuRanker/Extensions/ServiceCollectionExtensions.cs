0ade7d56e32bf9029beb60a1101a879633f6615e
Frederik Baun Hansen <frederik.baun@dentsuaegis.com>
2020-02-21 10:57:23 +0100

    Add dependency injection

---
+++ AccuRanker/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace DentsuDataLab.AccuRanker.Extensions
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Services.Authorization;
+    using Services.Endpoints;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAccuRankerServices(this IServiceCollection services)
+        {
+            services.AddSingleton<AccuRankerClientStore>();
+
+            services.AddTransient<AccuRankerAuthorizationService>();
+
+            services.AddTransient<AccountService>();
+            services.AddTransient<DomainService>();
+            services.AddTransient<KeywordService>();
+
+            services.AddHttpClient<AccountService>();
+            services.AddHttpClient<DomainService>();
+            services.AddHttpClient<KeywordService>();
+        }
+    }
+}
